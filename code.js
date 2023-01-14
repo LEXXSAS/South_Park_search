9ab5fd8b0413b83ef96469b1d1437aab499c5bb9
Alexey <93075959+LEXXSAS@users.noreply.github.com>
2023-01-14 19:10:58 +0300

    Update code.js

--- code.js
+++ code.js
@@ -33,14 +33,14 @@ fetch(api)
     function displayOptions() {
         
         
-        searchInput.onchange = function() {
-        let name = this.value;
+   
+        let name = searchInput.value;
         let per_name =/^[А-Я]$/i;
         if(name == !per_name && name !== ''){
         let text = "Разрешены только русские буквы";
         alert(text);
         } 
-        }
+
 
         const options = getOptions(this.value, characters);
         
